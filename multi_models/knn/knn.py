196f6aab7ac50e2cfba09ac89497af6f16e2a496
uchihashi_k <uchihashi.kenshi@gmail.com>
2016-07-05 11:04:52 +0900

    #7 knn用意した

--- multi_models/knn/knn.py
+++ multi_models/knn/knn.py
@@ -3,40 +3,45 @@ import numpy
 import fastdtw
 
 
-def get_nearest_n_dtw(self, train, label, test):
-    """
-    :param train: Training dataset. Must be pandas object.
-    :param label: Training label.
-    :param test: Some point of test data. Must be numpy array object.
-    :return: The nearest points of training dataset (with DTW metrics).
-    """
-    nn_dist_array, nn_ts_ls, nn_label_array = numpy.array([]), [], numpy.array([])
-    te_ele = numpy.array(test).reshape(-1, 1)
+class KNearestNeighbor(object):
 
-    for i, tr_ele in enumerate(train):
+    def __init__(self):
+        pass
 
-        # sys.stdout.write('\r%d' % i)
-        # sys.stdout.flush()
+    def get_nearest_n_dtw(self, train, label, test):
+        """
+        :param train: Training dataset. Must be pandas object.
+        :param label: Training label.
+        :param test: Some point of test data. Must be numpy array object.
+        :return: The nearest points of training dataset (with DTW metrics).
+        """
+        nn_dist_array, nn_ts_ls, nn_label_array = numpy.array([]), [], numpy.array([])
+        te_ele = numpy.array(test).reshape(-1, 1)
 
-        tr_ele_ls = tr_ele.tolist()
-        tr_ele = numpy.array(tr_ele).reshape(-1, 1)
-        dist, path = fastdtw(te_ele, tr_ele, dist=euclidean)
+        for i, tr_ele in enumerate(train):
 
-        if len(nn_dist_array) < self.nn_num:
-            nn_dist_array = numpy.append(nn_dist_array, dist)
-            nn_ts_ls.append(tr_ele_ls)
-            nn_label_array = numpy.append(nn_label_array, label[i])
-        elif numpy.max(nn_dist_array) > dist:
+            # sys.stdout.write('\r%d' % i)
+            # sys.stdout.flush()
 
-            if numpy.max(nn_dist_array) < self.max_dist:
-                break
+            tr_ele_ls = tr_ele.tolist()
+            tr_ele = numpy.array(tr_ele).reshape(-1, 1)
+            dist, path = fastdtw(te_ele, tr_ele, dist=euclidean)
 
-            max_ind = numpy.argmax(nn_dist_array)
-            nn_dist_array[max_ind] = dist
-            nn_ts_ls[max_ind] = tr_ele_ls
-            nn_label_array[max_ind] = label[i]
-        else:
-            continue
-    nn_ts_array = numpy.array(nn_ts_ls)
+            if len(nn_dist_array) < self.nn_num:
+                nn_dist_array = numpy.append(nn_dist_array, dist)
+                nn_ts_ls.append(tr_ele_ls)
+                nn_label_array = numpy.append(nn_label_array, label[i])
+            elif numpy.max(nn_dist_array) > dist:
 
-    return nn_dist_array, nn_ts_array, nn_label_array
+                if numpy.max(nn_dist_array) < self.max_dist:
+                    break
+
+                max_ind = numpy.argmax(nn_dist_array)
+                nn_dist_array[max_ind] = dist
+                nn_ts_ls[max_ind] = tr_ele_ls
+                nn_label_array[max_ind] = label[i]
+            else:
+                continue
+        nn_ts_array = numpy.array(nn_ts_ls)
+
+        return nn_dist_array, nn_ts_array, nn_label_array
